74e4271d83e9666a8ce078963bc72b13c490cf68
SachJay <jayaweerasachin@gmail.com>
2021-04-05 21:53:47 -0400

    updates UI

--- client/src/components/screens/Login/LoginPage.js
+++ client/src/components/screens/Login/LoginPage.js
@@ -3,16 +3,20 @@ import { Box, Button, TextInput } from "grommet";
 import { Link } from "@material-ui/core"
 import { useHistory } from "react-router-dom";
 
-export default function LoginPage() {
+import { useDispatch } from 'react-redux';
+import loggedIn from 'C:/Users/SachJ/Github/CIS3750_JS/CIS3750_JS/client/src/components/state/actions/loginAction.js';
 
+
+export default function LoginPage() {
+  const dispatch = useDispatch();
   const history = useHistory();
 
   const routeChange = () =>{ 
-    if (email === "admin@fitnova.com" && password === "password") {
-      let path = `newPath`; 
+   // if (email === "admin@fitnova.com" && password === "password") {
+      dispatch(loggedIn());
+      let path = ``; 
       history.push(path);
-    }
-
+ //   }
   }
 
   const [email, setEmail] = React.useState('');
@@ -20,50 +24,56 @@ export default function LoginPage() {
 
   return (
     <div>
-        <p>Replace this with code</p>
-        <h1>Welcome</h1>
-        <img src="fitnova_icon.png"></img><br/>
+       
 
-        <Box align="center" background="white">
-        <TextInput
-          placeholder="Email"
-          value={email}
-          onChange={event => setEmail(event.target.value)}
-        />
+        <Box display="flex" justifyContent="center"  background="white" style = {{width: 600}}>
+          <h1>Welcome</h1>
+          <img src="fitnova_icon.png"></img><br/>
+          <TextInput
+            placeholder="Email"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+            style = {{width: 600}}
+          />
 
-        <TextInput
-          placeholder="Password"
-          type="password"
-          value={password}
-          onChange={event => setPassword(event.target.value)}
-        />
+          <TextInput
+            placeholder="Password"
+            type="password"
+            value={password}
+            style = {{width: 600}}
+            onChange={event => setPassword(event.target.value)}
+          />
 
-        <Button
-            label="Login"
-            primary 
-            onClick={() => routeChange()}
-        />
+          <Button
+              label="Login"
+              primary 
+              style = {{width: 100}}
+              onClick={() => routeChange()}
+          />
+
+          <Link
+            component="button"
+            variant="body2"
+            onClick={() => {
+              alert("Take me to reset password"); //not going to happen
+            }}
+          >
+            Forgot your password?
+          </Link><br/>
+
+          <Link
+            component="button"
+            variant="body2"
+            onClick={() => {
+              alert("Take me to register"); //not going to happen
+            }}
+          >
+            Don’t have an account? Sign up!
+          </Link><br/>
+
         </Box>
 
-        <Link
-          component="button"
-          variant="body2"
-          onClick={() => {
-            alert("Take me to reset password"); //TODO
-          }}
-        >
-          Forgot your password?
-        </Link><br/>
-
-        <Link
-          component="button"
-          variant="body2"
-          onClick={() => {
-            alert("Take me to register"); //TODO
-          }}
-        >
-          Don’t have an account? Sign up!
-        </Link><br/>
+        
     </div>
   );
 }
