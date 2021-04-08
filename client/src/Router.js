13a51f4b608ffcbe0881c3ca07eac9f7f840fbd7
SachJay <jayaweerasachin@gmail.com>
2021-04-07 22:33:26 -0400

    added logic to not show navbar on checkout

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { useLocation, BrowserRouter, Switch, Route } from "react-router-dom";
 import LoginPage from "./components/screens/Login/LoginPage";
 import LandingPage from "./components/screens/LandingPage/LandingPage";
 import Products from "./components/screens/Products/Products";
@@ -8,10 +8,18 @@ import InventoryPage from "./components/screens/InventoryPage/InventoryPage";
 import Navbar from "./components/utils/Navbar/Navbar";
 
 export default function Router() {
+  let navbar;
+
+  if(window.location.pathname.includes("checkout")) {
+    navbar = <div></div>
+  } else {
+    navbar = <div><Navbar /></div>;
+  }
+
   return (
     <div>
       <BrowserRouter>
-        <Navbar />
+        {navbar}
         <Switch>
           <Route path="/" exact component={LandingPage} />
           <Route path="/products" exact component={Products} />
